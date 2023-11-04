9d89726851c7dc2a0a03ab985b5ef4f51275ca1e
olzh2102 <olzhas2102@yahoo.com>
2023-11-04 08:31:55 +0100

    feat: kafka producer, logging middleware, draft kafka consumer

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -10,7 +10,10 @@ import (
 )
 
 func main() {
-	recv := NewDataReceiver()
+	recv, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
 }
@@ -18,12 +21,28 @@ func main() {
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
+	prod  DataProducer
 }
 
-func NewDataReceiver() *DataReceiver {
+func NewDataReceiver() (*DataReceiver, error) {
+	var (
+		p          DataProducer
+		err        error
+		kafkaTopic = "obudata"
+	)
+	p, err = NewKafkaProducer(kafkaTopic)
+	if err != nil {
+		return nil, err
+	}
+	p = NewLogMiddleware(p)
 	return &DataReceiver{
 		msgch: make(chan types.OBUData, 128),
-	}
+		prod:  p,
+	}, nil
+}
+
+func (dr *DataReceiver) produceData(data types.OBUData) error {
+	return dr.prod.ProduceData(data)
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -42,13 +61,15 @@ func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
 
 func (dr *DataReceiver) wsReceiveLoop() {
 	fmt.Println("NEW OBU connected client connected!")
+
 	for {
 		var data types.OBUData
 		if err := dr.conn.ReadJSON(&data); err != nil {
 			log.Println("read error: ", err)
 			continue
 		}
-		fmt.Printf("received OBU data from [%d] :: <lat %.2f, long %.2f> \n", data.OBUID, data.Lat, data.Long)
-		dr.msgch <- data
+		if err := dr.produceData(data); err != nil {
+			fmt.Println("kafka produce error", err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/olzh2102/toll-calculator
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
