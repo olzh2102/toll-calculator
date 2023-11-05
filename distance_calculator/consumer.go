f720de60b08c7279227cb20929eff8f4198727a1
olzh2102 <olzhas2102@yahoo.com>
2023-11-05 17:11:35 +0100

    distance calculator service

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -1,5 +1,63 @@
 package main
 
-type DataConsumer interface {
-	ConsumeData()
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/olzh2102/toll-calculator/types"
+	"github.com/sirupsen/logrus"
+)
+
+// * this can also be called KafkaTransport.
+type KafkaConsumer struct {
+	consumer    *kafka.Consumer
+	isRunning   bool
+	calcService CalculatorServicer
+}
+
+func NewKafkaConsumer(topic string, svc CalculatorServicer) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	c.SubscribeTopics([]string{topic}, nil)
+
+	return &KafkaConsumer{
+		consumer:    c,
+		calcService: svc,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("Kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+	for c.isRunning {
+		// passing -1 so we can wait indefinitely, and not error timeout
+		msg, err := c.consumer.ReadMessage(-1)
+		if err != nil {
+			logrus.Errorf("kafka consume error %s", err)
+			continue
+		}
+		var data types.OBUData
+		if err := json.Unmarshal(msg.Value, &data); err != nil {
+			logrus.Errorf("JSON serialization error: %s", err)
+			continue
+		}
+		distance, err := c.calcService.CalculateDistance(data)
+		if err != nil {
+			logrus.Errorf("calculation error: %s", err)
+			continue
+		}
+		fmt.Printf("distance %.2f\n", distance)
+	}
 }
---
+++ distance_calculator/service.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"math"
+
+	"github.com/olzh2102/toll-calculator/types"
+)
+
+type CalculatorServicer interface {
+	CalculateDistance(types.OBUData) (float64, error)
+}
+
+type CalculatorService struct {
+	points [][]float64
+}
+
+func NewCalculatorService() CalculatorServicer {
+	return &CalculatorService{
+		points: make([][]float64, 0),
+	}
+}
+
+func (s *CalculatorService) CalculateDistance(data types.OBUData) (float64, error) {
+	distance := 0.0
+	if len(s.points) > 0 {
+		prevPoint := s.points[len(s.points)-1]
+		distance = calculateDistance(prevPoint[0], prevPoint[1], data.Lat, data.Long)
+	}
+	s.points = append(s.points, []float64{data.Lat, data.Long})
+	return distance, nil
+}
+
+func calculateDistance(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
+}
